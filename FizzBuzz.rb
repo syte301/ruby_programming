1355d41c70bc4fcb761801726469f4150873d441
syte301 <73173112+syte301@users.noreply.github.com>
2024-07-23 19:56:30 +0600

    Create FizzBuzz.rb

---
+++ FizzBuzz.rb
@@ -0,0 +1,18 @@
+#FizzBuzz in ruby
+
+def fizzbuzz(n)
+  (1..n).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
+end
+
+// Example usage
+fizzbuzz(20)
